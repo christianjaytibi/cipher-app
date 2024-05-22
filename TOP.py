0d686091b3993856864778fa2a0d6413a54ba538
Christian Jay Tibi <christianjaytibi@gmail.com>
2024-05-22 18:51:38 +0800

    added AES code snippet

--- TOP.py
+++ TOP.py
@@ -112,6 +112,28 @@ def main() -> None:
             ```
         """
     )
+    col3.divider()
+
+    col3.page_link(page="pages/3_AES.py",
+                   label=":green[AES]")
+    col3.markdown(
+        """
+            ```python
+            import os
+            from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+
+            >>> key = os.urandom(32)
+            >>> iv = os.urandom(16)
+            >>> cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+            >>> encryptor = cipher.encryptor()
+            >>> ct = encryptor.update(b"a secret message") + encryptor.finalize()
+            >>> decryptor = cipher.decryptor()
+            >>> decryptor.update(ct) + decryptor.finalize()
+            >>> b'a secret message'
+            ```
+        """
+    )
 
 
 if __name__ == "__main__":
